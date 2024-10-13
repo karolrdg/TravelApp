0b8d917f6c4ae2eed9fa84078a73aafac49769f2
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-10-12 21:42:03 -0300

    Refactor Hero component to adjust map positioning and container width

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,13 +5,13 @@ import { HERO } from "@/constants";
 const Hero = () => {
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
-      <div className="hero-map" />
+      <div className="hero-map mt-10"/>
 
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image
           src="/camp.svg"
           alt="camp"
-          width={50}
+          width={50}  
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
@@ -56,8 +56,8 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="relative flex flex-1 items-start">
-        <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
+      <div className="relative flex flex-1 items-start justify-center">
+        <div className="relative z-20 flex w-[300px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
               <p className="regular-16 text-gray-20">Location</p>
