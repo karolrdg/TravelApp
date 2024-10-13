6438f5ac5e6502cbf2f320e7f4d6de09ebc08fcf
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-10-12 21:41:40 -0300

    refactor: Remove unused background image in tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -31,7 +31,6 @@ module.exports = {
       },
       backgroundImage: {
         'bg-img-1': "url('/img-1.png')",
-        'bg-img-2': "url('/img-2.png')",
         'feature-bg': "url('/feature-bg.png')",
         pattern: "url('/pattern.png')",
         'pattern-2': "url('/pattern-bg.png')",
