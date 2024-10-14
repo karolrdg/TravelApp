cab105a5869726d0fac024ddadd3a2983202dd92
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-10-14 19:20:19 -0300

    Refactor GetApp component to adjust layout and add responsive styles

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -5,12 +5,13 @@ import Image from "next/image";
 const GetApp = () => {
   return (
     <section className="flexCenter w-full flex-col pb-[100px]">
-      <div className="get-app">
-        <div className="z-20 flex w-full flex-1 flex-col items-start justify-center gap-12">
-          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">
+      <div className="get-app flex flex-col items-center lg:flex-row lg:justify-between">
+        {/* Texto e botões */}
+        <div className="z-20 flex w-full flex-1 flex-col items-center gap-8">
+          <h2 className="bold-40 lg:bold-52 xl:max-w-[500px] text-center">
             Get for free now!
           </h2>
-          <p className="regular-16 text-gray-10">
+          <p className="regular-16 text-gray-10 text-center">
             Available on iOS and Android
           </p>
           <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
@@ -31,12 +32,14 @@ const GetApp = () => {
           </div>
         </div>
 
-        <div className="flex flex-1 items-center justify-end">
+        {/* Imagem do telefone */}
+        <div className="flex items-center justify-center w-full lg:w-auto">
           <Image
             src="/phones.png"
             alt="phones"
             width={550}
-            height={870}
+            height={700}
+            className="lg:mt-10 lg:w-[415px] lg:h-[600px]"
           />
         </div>
       </div>
