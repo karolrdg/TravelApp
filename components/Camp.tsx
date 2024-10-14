832e2160869a502f50be4bd608a56ccf289de75d
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-10-14 19:20:26 -0300

    Refactor Camp component to remove unused CampSite and adjust background image styling

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -17,7 +17,7 @@ const CampSite = ({
 }: CampProps) => {
   return (
     <div
-      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat lg:rounded-r-5xl 2xl:rounded-5xl`}
+      className={`h-full w-full min-w-[1100px] ${backgroundImage} bg-cover bg-no-repeat`}
     >
       <div className="flex h-full flex-col items-start justify-between p-6 lg:px-20 lg:py-10">
         <div className="flexCenter gap-4">
