4b29700b00be1d5245c1eabdb7ed3e6273220718
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-10-16 15:41:08 -0300

    style: Hero component layout and styles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,21 +4,29 @@ import { HERO } from "@/constants";
 
 const Hero = () => {
   return (
-    <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
-      <div className="hero-map mt-10"/>
-
-      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+    <section className="max-container padding-container flex flex-col gap-40 py-10 pb-32 md:gap-28 lg:py-20 lg:relative xl:flex-row overflow-hidden">
+      <div className="hero-map" />
+      
+      {/* Texto e botões */}
+      <div className="relative z-20 flex flex-1 flex-col py-7 px-2 sm:w-full md:w-4/5 lg:w-3/4 xl:w-2/3 2xl:w-3/4">
+        {/* Imagem do acampamento */}
         <Image
           src="/camp.svg"
           alt="camp"
-          width={50}  
+          width={50}
           height={50}
-          className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
+          className="absolute left-[-4px] top-[2px] w-10 lg:w-[50px]"
         />
-        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        
+        {/* Título */}
+        <h1 className="bold-52 lg:bold-64">Putuk Truno Camp Area</h1>
+        
+        {/* Descrição */}
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
           {HERO.description}
         </p>
+        
+        {/* Avaliações */}
         <div className="my-11 flex flex-wrap gap-5">
           <div className="flex items-center gap-2">
             {Array(5)
@@ -36,11 +44,11 @@ const Hero = () => {
 
           <p className="bold-16 lg:bold-20 text-blue-70">
             198k
-            <span className="regular-16 lg:regular-20 ml-1">
-              Excellent Reviews
-            </span>
+            <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
           </p>
         </div>
+
+        {/* Botões */}
         <div className="flex flex-col w-full gap-3 sm:flex-row">
           <Button
             type="button"
@@ -56,10 +64,11 @@ const Hero = () => {
         </div>
       </div>
 
+      {/* Informações adicionais sobre a localização */}
       <div className="relative flex flex-1 items-start justify-center">
         <div className="relative z-20 flex w-[350px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
-            <div className="flexBetween">
+            <div className="flex justify-between">
               <p className="regular-16 text-gray-20">Location</p>
               <Image
                 src="/close.svg"
@@ -71,17 +80,13 @@ const Hero = () => {
             <p className="bold-20 text-white">Aguas Calientes</p>
           </div>
 
-          <div className="flexBetween">
+          <div className="flex justify-between">
             <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">
-                Distance
-              </p>
+              <p className="regular-16 block text-gray-20">Distance</p>
               <p className="bold-20 text-white">173.28 mi</p>
             </div>
             <div className="flex flex-col">
-              <p className="regular-16 block text-gray-20">
-                Elevation
-              </p>
+              <p className="regular-16 block text-gray-20">Elevation</p>
               <p className="bold-20 text-white">2.040 km</p>
             </div>
           </div>
@@ -91,11 +96,4 @@ const Hero = () => {
   );
 };
 
-/*type HeroProps = {
-  description: string;
-};
-
-const HeroProps = ({ description }: HeroProps) => {
-  return <p>{description}</p>;
-};*/
 export default Hero;
