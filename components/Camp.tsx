8cf6725ac306b00f02cccef0a71d7eb35490a291
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-10-22 18:01:39 -0300

    style: Adjust padding and layout in Camp component

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -62,8 +62,8 @@ const CampSite = ({
 
 const Camp = () => {
   return (
-    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+    <section className="2xl:max-container relative flex flex-col py-10 xl:mb-15">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 lg:h-[400px] xl:h-[640px]">
         <CampSite
           backgroundImage="bg-bg-img-1"
           title="Putuk Truno Camp"
