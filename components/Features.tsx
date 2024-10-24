b9ec990279bfdbd6a9db228b10fae3c14403d78e
Karoline Rodrigues <87062322+karolrdg@users.noreply.github.com>
2024-10-23 21:28:21 -0300

    style: Refactor Hero component styles in Guide

--- components/Features.tsx
+++ components/Features.tsx
@@ -4,8 +4,8 @@ import React from "react";
 
 const Features = () => {
   return (
-    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      <div className="max-container padding-container relative w-full flex justify-end">
+    <section className="flex-col flexCenter bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative flex justify-end overflow-hidden">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src="/phone.png"
@@ -18,7 +18,7 @@ const Features = () => {
 
         <div className="z-20 flex w-full flex-col lg:w-[60%]">
           <div className="relative">
-            <h2 className="bold-40 lg:bold-64  text-center">
+            <h2 className=" bold-40 lg:bold-64  text-center lg:mt-8">
               Our Features
             </h2>
           </div>
@@ -50,7 +50,7 @@ const FeatureItem = ({ title, icon, description }: FeatureItem) => {
       <div className="rounded-full p-4 lg:p-7 bg-green-50">
         <Image src={icon} alt="map" width={28} height={28} />
       </div>
-      <h2 className="bold-2 lg:bold-32 mt-5 capitalize text-center">
+      <h2 className="bold-32 lg:bold-32 mt-5 capitalize text-center">
         {title}
       </h2>
       <p className="regular-16 mt-3 bg-white/80 text-gray-30 lg:mt-[10px] lg:bg-none text-center">
